d472c15dcf2607aa23ecd42dbb85309e8a177007
govltjsdnd24 <gragasolaf@gmail.com>
2023-09-05 17:07:32 +0900

    1

--- Homework/데일리 실습_Front-End_03_3_제공파일/js/cafe.js
+++ Homework/데일리 실습_Front-End_03_3_제공파일/js/cafe.js
@@ -1,3 +1,51 @@
+
+onload = function () {
+    let poll = localStorage.getItem("poll");
+    let pollBtnDiv = document.querySelector(".vote_button");
+    let pollViewDiv = document.querySelector(".vote");
+    console.log(pollViewDiv);
+
+    let pollContent;
+
+    if (poll) {
+        // 진행중인 투표가 있다면.
+        let pollJson = JSON.parse(poll);
+        let pollView = `
+            <h3 style="margin:auto; text-align:center;">[ 당신의 선택 ]</h3>
+            <h4>${pollJson.question}</h4>
+            <div class="poll-answer-list">
+         `;
+        let answers = pollJson.answers;
+        for (let answer of answers) {
+            pollView += `
+            <label><input type="radio" name="poll-answer" value="${answer}" /> ${answer}</label><br>
+            `;
+        }
+        let start_date = pollJson.startDate;
+        let end_date = pollJson.endDate;
+
+        pollView += `
+        </div>
+        <div class="poll-btn-list" style="margin-right:10px; text-align:right;">
+          <button type="button" id="polling-btn" class="button btn-primary" onclick="javascript:polling();">투표하기</button>
+          <button class="button btn-primary" >결과보기</button>
+        </div>
+        <div class="poll-date" style="font-size:14px"><b>투표기간 : ${start_date} ~ ${end_date}</b></div>
+        `;
+        pollViewDiv.innerHTML = pollView;
+
+        pollBtnDiv.style.display = "none";
+        pollViewDiv.style.display = "";
+    } else {
+        // 진행중인 투표가 없다면.
+        pollContent = `진행중인 투표가 없습니다.`;
+        pollViewDiv.innerHTML = pollContent;
+
+        pollBtnDiv.style.display = "";
+        pollViewDiv.setAttribute("style", "text-align: center; display: block;");
+    }
+}
+
 function login() {
     while (txt != "ssafy") {
         var txt = prompt("아이디입력", "아이디를 입력해주세요.");
@@ -48,6 +96,7 @@ function allSlide(command) {
 function pollMake() {
     window.open("./pollmake.html", "winname", "width=420, height=280");
 }
+
 var counter = 1;
 function addAnswer() {
     let answer = document.createElement("div");
@@ -75,4 +124,71 @@ function removeElement(index) {
     let el = document.getElementById(index);
     document.getElementById("poll_answer_list").removeChild(el);
     // counter--;
+}
+
+function makePoll() {
+    let sdate = document.querySelector("#start_date").value;
+    let start_date = sdate.substring(2, 4) + "." + sdate.substring(5, 7) + "." + sdate.substring(8, 10);
+    let edate = document.querySelector("#end_date").value;
+    let end_date = edate.substring(2, 4) + "." + edate.substring(5, 7) + "." + edate.substring(8, 10);
+    
+    if (!start_date) {
+        alert("시작일자는 필수!!!");
+        return;
+    }
+    else if (!end_date) {
+        alert("종료일자는 필수!!!");
+        return;
+    }
+
+    let question = document.querySelector("#question").value;
+    if (!question) {
+        alert("질문은 필수!!!");
+        return;
+    }
+
+    let answers = document.getElementsByName("answer");
+
+    for (const answer of answers) {
+        if (!answer.value) {
+        alert("답변 필수!!!");
+        return;
+        }
+    }
+
+    let answerArr = [];
+    for (const answer of answers) {
+        answerArr.push(answer.value);
+    }
+
+    let poll = {
+        question: question,
+        startDate: start_date,
+        endDate: end_date,
+        answers: answerArr,
+    };
+
+    if (confirm("투표를 생성하시겠습니까?")) {
+        localStorage.setItem("poll", JSON.stringify(poll));
+        opener.location.reload();
+        self.close();
+    }
+}
+
+function polling() {
+    let id = prompt("아이디입력", "아이디");
+      console.log(id);
+      let pwd = prompt("비밀번호입력", "비밀번호");
+      console.log(pwd);
+  
+      let answers = document.getElementsByName("poll-answer");
+      let val = "";
+      for (let answer of answers) {
+        if (answer.checked) {
+          val = answer.value;
+          break;
+        }
+      }
+      if (val) alert(val + "을 선택!!!!");
+      else alert("답변 항목 하나를 선택 해 주세요!!!");
 }
